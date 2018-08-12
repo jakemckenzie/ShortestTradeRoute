eb9fa7bd0399561b2ce061c517407a4a73976ebd
jakemckenzie <jake314@uw.edu>
2018-08-12 16:55:43 -0700

    path created now onto graph

---
+++ Path.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Path{
+    List<Edge> path;
+    int cost;
+    public Path(int size){
+        path = new ArrayList<Edge>(size);
+        cost = 0;
+    }
+    public void addEdge(Edge e){
+        if(!e.source.equals(e.destination)){
+            int size = path.size();
+            if(path.size() < 1){
+                path.add(e);
+                cost+=e.weight;
+            }else if(path.get(0).source.node == e.destination.node){
+                path.add(0, e);
+                cost+=e.weight;
+            } else if(path.get(size - 1).destination.node == e.source.node){
+                if(!path.contains(e)){
+                    path.add(e);
+                    cost+=e.weight;	
+                }  
+            }
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        if (this.path.size() < 0) return sb.toString(); 
+        sb.append("(");
+        for (Edge e : path){
+            sb.append(e.source.node);
+            sb.append(", ");
+        }
+        sb.append(path.get(path.size() - 1).destination.node);
+        sb.append(", ");
+        sb.append(")");
+        return sb.toString();
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        Path p = (Path) o;
+        return (this.cost == p.cost) ? this.path.equals(p.path) ? true : false : false;
+    }
+
+    public boolean flagger(Edge e){
+        int size = path.size();
+        return (path.size() < 1) ? true : (path.get(size-1).destination.node == e.source.node) ? true : false;
+    }
+}
